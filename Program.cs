e5d093d8f6150ba11f4fbefb5b455587eb38062e
babakovn <babakovn@gmail.com>
2022-12-04 08:48:23 +0300

    Исправлены строки и столбцы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] GetRandomArray(int rows, int column)
+{
+    int[,] arr = new int[rows, column];
+    Random a = new Random();
+    for(int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < column; j++)
+            {
+                arr[i,j] = a.Next();
+            }
+        }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+        {
+            for(int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write($"{arr[i,j]} ");
+            }
+        Console.WriteLine();
+        }
+}
+
+
+
+//
+
+int[,] arr = GetRandomArray(3,4);
+PrintArray(arr);
